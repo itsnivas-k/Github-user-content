7fd903cb8417ec7aff8386c458ec6d18ab2ca2d0
Nivas Kanniah <nivascse2020@mvit.edu.in>
2022-04-25 21:57:52 +0530

    Add files via upload

---
+++ Usercontent.py
@@ -0,0 +1,11 @@
+
+
+# your link goes here
+link = "https://github.com/itsnivas-k/Covid-19-Data-Analysis/blob/main/Covid_data_analysis.ipynb"
+
+
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
+
+# example output link:
+#https://raw.githubusercontent.com/itsnivas-k/Covid-19-Data-Analysis/main/Covid_data_analysis.ipynb
+
